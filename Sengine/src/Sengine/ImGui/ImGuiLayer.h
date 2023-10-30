a0bbd3e4c7d4043809f5bba97129d087e4ac7095
나성민 <83560500+longnu@users.noreply.github.com>
2023-10-31 05:55:54 +0900

    feat : imgui event

--- Sengine/src/Sengine/ImGui/ImGuiLayer.h
+++ Sengine/src/Sengine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Sengine/Layer.h"
 
+#include "Sengine/Events/ApplicationEvent.h"
+#include "Sengine/Events/KeyEvent.h"
+#include "Sengine/Events/MouseEvent.h"
+
 namespace Sengine {
 
 	class SENGINE_API ImGuiLayer : public Layer
@@ -14,16 +18,17 @@ namespace Sengine {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
-
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
-
 	};
 
-
 }
-
-class ImGuiLayer
-{
-};
-
