1c2f704fea845b22c3190c287db54151dc99ea6a
나성민 <83560500+longnu@users.noreply.github.com>
2023-10-31 19:43:41 +0900

    feat : input polling

--- Sengine/src/Sengine/Application.cpp
+++ Sengine/src/Sengine/Application.cpp
@@ -6,6 +6,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Sengine {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -62,6 +64,9 @@ namespace Sengine {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			SE_CORE_TRACE("{0}, {1}",x,y);
 
 			m_Window->OnUpdate();
 		}
